84d1e41f91ff1f404a7e1b0dd2db0500afbcf294
ShvetGhare <ghareshvet@gmail.com>
2025-06-02 11:40:29 +0530

    7 commit

--- backend/middlewares/verifyToken.js
+++ backend/middlewares/verifyToken.js
@@ -1,16 +1,23 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
+  if (!authHeader)
+    return res
+      .status(403)
+      .json({ message: "Access Denied: No Token Provided" });
+
+  // Expected format: "Bearer <token>"
+  const token = authHeader.split(" ")[1];
   if (!token)
-    return res.status(403).json({ message: "Access Denied: No Token" });
+    return res.status(403).json({ message: "Access Denied: Token Missing" });
 
   try {
-    const verified = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
-    req.user = verified;
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified; // Add user data from token to request
     next();
   } catch (err) {
-    res.status(401).json({ message: "Invalid Token" });
+    res.status(401).json({ message: "Invalid or Expired Token" });
   }
 };
 
